812432b3d0b152aa796074290f5e9db08a37240a
Bernabe19 <bernasanchezsos4@gmail.com>
2023-10-11 20:20:19 +0200

    minmax funcionando pendiente de arreglar

--- engine/comun.py
+++ engine/comun.py
@@ -4,8 +4,8 @@ import numpy as np
 from rich import print
 from copy import deepcopy
 
-from minmax import minmax
-from alfabeta import alfabeta
+# from minmax import minmax
+# from alfabeta import alfabeta
 
 
 main_board = [['-', 'X', '-'],
@@ -14,14 +14,17 @@ main_board = [['-', 'X', '-'],
 
 main_board = [['-' for _ in range(19)] for _ in range(19)]
 main_board[0][0] = 'X'
-X_positions = set()
-X_positions.add((1,1))
+last_X_positions = set()
+last_X_positions.add((0,0))
+last_O_positions = set()
+last_O_positions.add((5,5))
 O_positions = set()
 
 # Se puede elegir el jugador con el modulo de una variable, si es par uno y si es impar el otro jugador
 turn = 0
 limit = 19
 depth = 1
+RANGO = 1
 
 def update_board(board, action):
     
@@ -46,16 +49,32 @@ def player(board):
         return 'O'
     
 
-def actions(board):
+
+def actions(board, array_tuplas, rango):
     '''
-    Input -> board
+    Input -> board, array_tuplas, rango
     Return -> set de movimientos posibles {(i,j), (i,j), (i,j)}
     '''
     out_set = set()
-    for i, row in enumerate(board):
-        for j, col in enumerate(row):
-            if col == '-':
-                out_set.add((i, j))
+    # rango * 2 + 1
+    recorrido = rango * 2 + 1
+    if(array_tuplas):
+        for tupla in array_tuplas:
+            print(type(tupla[0]))
+            print(tupla[1])
+            i_inicial = tupla[0] - rango
+            j_inicial = tupla[1] - rango
+            
+            for i in range(recorrido):
+                for j in range(recorrido):
+                    if(not is_oponent_or_border(board,(i + i_inicial, j + j_inicial)) and board[i + i_inicial][j + j_inicial] != player(board)):
+                        out_set.add((i + i_inicial, j + j_inicial))
+
+                    
+        # for i, row in enumerate(board):
+        #     for j, col in enumerate(row):
+        #         if col == '-':
+        #             out_set.add((i, j))
     return out_set
 
 
@@ -133,57 +152,67 @@ def utility(board):
     '''
     if terminal(board):
         if winner(board) == 'X':
-            return 1
-        if winner(board) == 'O':
-            return -1
-    return 0
+            return float("inf")
+        elif winner(board) == 'O':
+            return float("-inf")
+        else:
+            return hmove_evaluation(board)
 
 def is_oponent_or_border(board, position: tuple):
     if player(board) == 'O':
         opponnent = 'X'
     else: 
         opponnent = 'O'
-
+    
     try:
-        if board[position[0]][position[1]] == opponnent:
+        if board[position[0]][position[1]] == opponnent or position[0] < 0 or position[1] < 0:
             return True
-    except IndexError:
+    except:
         return True
+    
     return False
 
 
 
-def evaluate(board,threat_size,opponnent):
+def has_win_or_threat(board,threat_size,player):
     for row in board:
-        if ''.join(row).count(opponnent * threat_size) > 0:
+        if ''.join(row).count(player * threat_size) > 0:
             return True
 
-    # Verificar victoria vertical
+    # Verificar amenaza vertical
     for col in range(len(board[0])):
         column = ''.join([row[col] for row in board])
-        if column.count(opponnent * threat_size) > 0:
+        if column.count(player * threat_size) > 0:
             return True
 
-    # Verificar victoria en diagonal (ascendente)
-    for i in range(len(board) - 5):
-        for j in range(len(board[0]) - 5):
+    # Verificar amenaza en diagonal (ascendente)
+    for i in range(len(board) - 4):
+        for j in range(len(board[0]) - 4):
             diagonal = ''.join([board[i + k][j + k] for k in range(threat_size)])
-            if diagonal.count(opponnent * threat_size) > 0:
+            if diagonal.count(player * threat_size) > 0:
                 return True
 
-    # Verificar victoria en diagonal (descendente)
-    for i in range(len(board) - 5):
-        for j in range(5, len(board[0])):
+    # Verificar amenaza en diagonal (descendente)
+    for i in range(len(board) - 4):
+        for j in range(4, len(board[0])):
             diagonal = ''.join([board[i + k][j - k] for k in range(threat_size)])
-            if diagonal.count(opponnent * threat_size) > 0:
+            if diagonal.count(player * threat_size) > 0:
                 return True
 
     return False
 
-def threat_space_search(board,threat_sizeopponnent):
+def threat_space_search(board, atacante):
+    '''
+    Recibe como parámetros el tablero y el atacante (jugador que jugará después)
+    Se define el tamaño de la amenaza (4/5) 
+    '''
     threat_size = 4
+    if atacante == 'O':
+        defensor = 'X'
+    else: 
+        defensor = 'O'
     
-    if evaluate(threat_size):
+    if has_win_or_threat(board, threat_size, defensor):
         return False 
     pass
 
@@ -192,6 +221,15 @@ def sliding_window():
 
 def defensive_slide_window():
     pass
+
+# def _calcule_e_dir(board, epsilon, e_dir, w, next_pos, i, j):
+#     if is_oponent_or_border(board, (i, j)):
+#         break
+#     elif board[i][j] == '-':
+#         e_dir *= epsilon
+#     elif board[i][j] == player(board):
+#         e_dir *= w[next_pos]
+#     return e_dir
 
 def hmove_evaluation(board, position: tuple):
     '''
@@ -218,6 +256,8 @@ def hmove_evaluation(board, position: tuple):
                         elif board[i][j] == player(board):
                             e_dir *= w[next_pos]
 
+                        #_calcule_e_dir(board, epsilon, e_dir, w, next_pos, i, j)
+
 
                     elif dir == 1:          # Diagonal ascendente
                         i = position[0] - next_pos
@@ -242,7 +282,7 @@ def hmove_evaluation(board, position: tuple):
 
                         
                     elif dir == 3:          # Diagonal descendente
-                        i = position[0] - next_pos
+                        i = position[0] + next_pos
                         j = position[1] - next_pos
                         if is_oponent_or_border(board, (i,j)):
                             break
@@ -298,16 +338,34 @@ def hmove_evaluation(board, position: tuple):
             e = e + e_dir
     return e
 
+def minmax(board, depth, action_p, maximazing = True):
+    '''
+    Input es el tablero del juego
+    Devuelve la jugada optima por el jugador
+    '''
+    if depth == 0 or terminal(board):
+        return utility(board,action_p)
+    if maximazing:
+        v = float('-inf')
+        for action in actions(board, set().add(action_p), RANGO):   
+            v = max(v, minmax(result(board, action), depth-1, False))
+        return v
+    else:
+        v = float('inf')
+        for action in actions(board, set().add(action_p), RANGO):
+            v = min(v, minmax(result(board, action), depth-1, True))
+        return v
     
 
 def best_move(board):
     val_max = float('-inf')
     best_action = None
 
-    for action in actions(board):
+    for action in actions(board, last_O_positions, RANGO):
         copy_board = deepcopy(board)
+        # Player simepre es X que es la IA
         copy_board[action[0]][action[1]] = player(board)
-        val = minmax(copy_board, depth, maximazing=False)
+        val = minmax(copy_board, depth, action, maximazing=False)
         if val_max < val:
             val_max = val
             best_action = action
@@ -334,20 +392,32 @@ def limpiar_terminal():
         # Si el sistema no es compatible, simplemente imprime caracteres de nueva línea para "limpiar" la pantalla
         print('\n' * 100)
 
-if __name__ == "__main__":
+def main():
+    global main_board
     print(main_board)
 
     while winner(main_board) == None:
         if player(main_board) == 'X':
             best = best_move(main_board)
+            last_X_positions.clear()
+            last_X_positions.add(best)
             print("MINMAX NEW", best)
             main_board[best[0]][best[1]] = player(main_board)
         else:
             i = input('Mov: ').split(' ')
+            action = tuple([int(el) for el in i])
+            last_O_positions.clear()
+            last_O_positions.add(action)
             if i[0] == 'e':
                 exit(0)
-            main_board = result(main_board, tuple([int(el) for el in i]))
+            main_board = result(main_board, action)
         limpiar_terminal()
         print_mat(main_board)
     else:
         print("FINISH")
+
+if __name__ == "__main__":
+    main()
+    # array_tuplas = [(0,0), (10,10)]
+    # print(actions(main_board, array_tuplas, 2))
+    # print(len(actions(main_board, array_tuplas, 2)))
